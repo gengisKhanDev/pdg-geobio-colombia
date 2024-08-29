c7bd2b6f15ecbce403653bd6990cb8467b005f19
harakiriOP <salvadorhidalgo100@gmail.com>
2024-08-29 11:04:18 -0500

    first commit

---
+++ imports/ui/components/Info.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import { useFind, useSubscribe } from 'meteor/react-meteor-data';
+
+export const Info = () => {
+
+  // if(isLoading()) {
+  //   return <div>Loading...</div>;
+  // }
+
+  return (
+    <div>
+      <h2>Learn Meteor!</h2>
+    </div>
+  );
+};
